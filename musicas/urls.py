e5998f1fe367c14520b2b50dc2ef8848c548dbc4
Augusto Campos <augustocampos@usp.br>
2024-11-13 15:30:48 -0300

    CRUD com views funcionais utilizando Django Form

--- musicas/urls.py
+++ musicas/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('search/', views.search_musica, name='search'),
     path('create/', views.create_musica, name='create'),
     path('<int:musica_id>/', views.detail_musica, name='detail'),
+    path('update/<int:musicas_id>/', views.update_musica, name='update'),
+    path('delete/<int:musicas_id>/', views.delete_musica, name='delete'),
 ]
--- musicas/views.py
+++ musicas/views.py
@@ -3,34 +3,63 @@ from .temp_data import musica_data
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import Musica
+from django.shortcuts import render, get_object_or_404
+
 
 def detail_musica(request, musica_id):
-    context = {'musica': musica_data[musica_id - 1]}
+    musicas = get_object_or_404(Musica, pk=musica_id)
+    context = {'musicas': musicas}
     return render(request, 'musicas/detail.html', context)
 
 def list_musica(request):
-    context = {"musica_list": musica_data}
+    musicas_list = Musica.objects.all()
+    context = {'musicas_list': musicas_list}
     return render(request, 'musicas/index.html', context)
 
 def search_musica(request):
     context = {}
     if request.GET.get('query', False):
-        context = {
-            "musica_list": [
-                m for m in musica_data
-                if request.GET['query'].lower() in m['name'].lower()
-            ]
-        }
+        search_term = request.GET['query'].lower()
+        musicas_list = Musica.objects.filter(name__icontains=search_term)
+        context = {"musicas_list": musicas_list}
     return render(request, 'musicas/search.html', context)
 
 def create_musica(request):
     if request.method == 'POST':
-        musica_data.append({
-            'name': request.POST['name'],
-            'release_year': request.POST['release_year'],
-            'poster_url': request.POST['poster_url']
-        })
+        musicas_name = request.POST['name']
+        musicas_release_year = request.POST['release_year']
+        musicas_poster_url = request.POST['poster_url']
+        musicas = Musica(name=musicas_name,
+                      release_year=musicas_release_year,
+                      poster_url=musicas_poster_url)
+        musicas.save()
         return HttpResponseRedirect(
-            reverse('musicas:detail', args=(len(musica_data), )))
+            reverse('musicas:detail', args=(musicas.id, )))
     else:
         return render(request, 'musicas/create.html', {})
+
+def update_musica(request, musicas_id):
+    musicas = get_object_or_404(Musica, pk=musicas_id)
+
+    if request.method == "POST":
+        musicas.name = request.POST['name']
+        musicas.release_year = request.POST['release_year']
+        musicas.poster_url = request.POST['poster_url']
+        musicas.save()
+        return HttpResponseRedirect(
+            reverse('musicas:detail', args=(musicas.id, )))
+
+    context = {'musicas': musicas}
+    return render(request, 'musicas/update.html', context)
+
+
+def delete_musica(request, musicas_id):
+    musicas = get_object_or_404(Musica, pk=musicas_id)
+
+    if request.method == "POST":
+        musicas.delete()
+        return HttpResponseRedirect(reverse('musicas:index'))
+
+    context = {'musicas': musicas}
+    return render(request, 'musicas/delete.html', context)
